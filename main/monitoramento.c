a23ee9c1f48750e1c48f01c7c5fe366b9a0a3aeb
Thiago Albuquerque <thiagoamoreira54@gmail.com>
2026-04-09 16:06:39 -0300

    Update monitoramento.c

--- main/monitoramento.c
+++ main/monitoramento.c
@@ -1,28 +1,29 @@
 #include "monitoramento.h"
-#include <stdio.h>
+#include <stdio.h>       
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-static int carros = 0;              
-static char mensagem[64];           
- 
+static int carros = 0;           //contador dos carros começa em 0
+static char mensagem[64];        //caixa que vai guardar a mensagem do total de carros
+
 void monitoramento_init(void) {
-    gpio_set_direction(SENSOR_GPIO, GPIO_MODE_INPUT);
+    gpio_set_direction(SENSOR_GPIO, GPIO_MODE_INPUT);  // Pino LDR como entrada
+    gpio_set_pull_mode(SENSOR_GPIO, GPIO_PULLDOWN_ONLY); // garante que o pino mantem 0 quando não tem carro
 }
- 
+
 const char* monitorar_carro(void) {
-    if (gpio_get_level(SENSOR_GPIO) == 1) {
-        carros++;
-        snprintf(mensagem, sizeof(mensagem),
+    if (gpio_get_level(SENSOR_GPIO) == 1) {     // "se o sensor tiver com baixa resistencia = carro detectado"
+        carros++;                               // incrementa o contador de carros em 1
+        snprintf(mensagem, sizeof(mensagem),    // coloca o numero de carros no texto
                  "Carro detectado! Total: %d carro(s)", carros);
- 
-        
-        while (gpio_get_level(SENSOR_GPIO) == 1) {
-            vTaskDelay(10 / portTICK_PERIOD_MS);
+
+        while (gpio_get_level(SENSOR_GPIO) == 1) {  // espera o LDR ficar com sinal 0, para não contar o mesmo carro várias vezes
+            vTaskDelay(pdMS_TO_TICKS(10));          
         }
- 
-        return mensagem;  
+
+        return mensagem;   // retorna a mensagem com o total de carros
     }
- 
-    return NULL;  
+
+    return NULL;   // retorna nada se não teve carro detectado
 }
