ca95573195520905601537cc8a93a92b9f6a3ffb
Thiago Albuquerque <thiagoamoreira54@gmail.com>
2026-04-09 16:06:53 -0300

    Update monitoramento.h

--- main/monitoramento.h
+++ main/monitoramento.h
@@ -1,17 +1,14 @@
-#ifndef MONITORAMENTO_H
+#ifndef MONITORAMENTO_H //cria a biblioteca 
 #define MONITORAMENTO_H
- 
+
 #include <stdio.h>
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
- 
-#define SENSOR_GPIO 36
- 
 
-void monitoramento_init(void);
- 
-const char* monitorar_carro(void);
- 
-#endif 
- 
+#define SENSOR_GPIO 25 //Pino do LDR
+
+void monitoramento_init(void);   // chama a função que configura o sensor
+const char* monitorar_carro(void); // chama a função que verifica o sensor
+
+#endif
