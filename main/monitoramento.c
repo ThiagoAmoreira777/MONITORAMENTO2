cc4f33a46646a23e3a958c91745942651538ec73
Thiago Albuquerque <thiagoamoreira54@gmail.com>
2026-05-01 09:55:35 -0300

    Refactor monitoramento.c for consistency

--- main/monitoramento.c
+++ main/monitoramento.c
@@ -7,12 +7,12 @@
 static int carros = 0;           //contador dos carros começa em 0
 static char mensagem[64];        //caixa que vai guardar a mensagem do total de carros
 
-void monitoramento_init(void) {
+void monitoramento_init() {
     gpio_set_direction(SENSOR_GPIO, GPIO_MODE_INPUT);  // Pino LDR como entrada
     gpio_set_pull_mode(SENSOR_GPIO, GPIO_PULLDOWN_ONLY); // garante que o pino mantem 0 quando não tem carro
 }
 
-const char* monitorar_carro(void) {
+const char* monitorar_carro() {
     if (gpio_get_level(SENSOR_GPIO) == 1) {     // "se o sensor tiver com baixa resistencia = carro detectado"
         carros++;                               // incrementa o contador de carros em 1
         snprintf(mensagem, sizeof(mensagem),    // coloca o numero de carros no texto
